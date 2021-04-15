d14a1f6f8ab22abd6f0b82791275c8261c9f825c
mprovenc <mkgprov@aol.com>
2021-04-15 09:31:52 -0400

    Add handling for device specific settings
    Update apply_kernel_report_settings test

--- library/kernel_report.py
+++ library/kernel_report.py
@@ -71,67 +71,82 @@ def get_sysctl_fields():
     for setting in SYSCTL_FIELDS:
         temp_dict = {"name": setting}
         temp_dict["value"] = safe_file_get_contents("/proc/sys/%s" % setting.replace(".","/"))
-        sysctl.append(temp_dict)
+        if temp_dict["value"]:
+            sysctl.append(temp_dict)
     return {"kernel_settings_sysctl": sysctl}
 
 def get_sysfs_fields():
     result = {}
+
+    # these are the settings not specific to any particular device
     result["kernel_settings_transparent_hugepages"] = re.findall(r'\[(\w+)\]', safe_file_get_contents('/sys/kernel/mm/transparent_hugepage/enabled'))[0]
     result["kernel_settings_transparent_hugepages_defrag"] = re.findall(r'\[(\w+)\]', safe_file_get_contents('/sys/kernel/mm/transparent_hugepage/defrag'))[0]
+    result["kernel_settings_cpu_min_perf_pct"] = safe_file_get_contents("/sys/devices/system/cpu/intel_pstate/min_perf_pct")
+    result["kernel_settings_cpu_max_perf_pct"] = safe_file_get_contents("/sys/devices/system/cpu/intel_pstate/max_perf_pct")
+    result["kernel_settings_cpu_no_turbo"] = safe_file_get_contents("/sys/devices/system/cpu/intel_pstate/no_turbo")
+    result["kernel_settings_avc_cache_threshold"] = safe_file_get_contents("/sys/fs/selinux/avc/cache_threshold")
+    result["kernel_settings_nf_conntrack_hashsize"] = safe_file_get_contents("/sys/module/nf_conntrack/parameters/hashsize")
+
+    kernel_settings_device_specific = {}
 
     # will collect a list of cpus associated to the template machine, but only use settings from the first one
     cpus = pyudev.Context().list_devices(subsystem="cpu")
     num_cpus = 0
     
+    kernel_settings_device_specific["kernel_settings_cpu_governor"] = []
+    kernel_settings_device_specific["kernel_settings_sampling_down_factor"] = []
     for cpu in cpus:
         num_cpus += 1
-        if 'kernel_settings_cpu_governor' not in result:
-            result["kernel_settings_cpu_governor"] = safe_file_get_contents('%s/cpufreq/scaling_governor' % cpu.sys_path)
-            result["kernel_settings_sampling_down_factor"] = safe_file_get_contents('/sys/devices/system/cpu/cpufreq/%s/sampling_down_factor' % result['kernel_settings_cpu_governor'])
+        kernel_settings_device_specific["kernel_settings_cpu_governor"].append({'device':cpu.sys_path, 'value':safe_file_get_contents('%s/cpufreq/scaling_governor' % cpu.sys_path)})
+        kernel_settings_device_specific["kernel_settings_sampling_down_factor"].append({'device':cpu.sys_path, 'value':safe_file_get_contents('/sys/devices/system/cpu/cpufreq/%s/sampling_down_factor' % kernel_settings_device_specific['kernel_settings_cpu_governor'][-1])})
     
     print("get_sysfs_fields: found %d cpus associated to the template machine" % num_cpus)
-
-    result["kernel_settings_cpu_min_perf_pct"] = safe_file_get_contents("/sys/devices/system/cpu/intel_pstate/min_perf_pct")
-    result["kernel_settings_cpu_max_perf_pct"] = safe_file_get_contents("/sys/devices/system/cpu/intel_pstate/max_perf_pct")
-    result["kernel_settings_cpu_no_turbo"] = safe_file_get_contents("/sys/devices/system/cpu/intel_pstate/no_turbo")
 
     # will collect a list of blocks associated to the template machine, but only use settings from the first one
     blocks = pyudev.Context().list_devices(subsystem="block")
     num_blocks = 0
 
+    kernel_settings_device_specific["kernel_settings_disk_elevator"] =[]
+    kernel_settings_device_specific["kernel_settings_disk_read_ahead_kb"] = []
+    kernel_settings_device_specific["kernel_settings_disk_scheduler_quantum"] = []
+
     for block in blocks:
         num_blocks += 1
-        if 'kernel_settings_disk_elevator' not in result:
-            result["kernel_settings_disk_elevator"] = re.findall(r'\[(\w+)\]', safe_file_get_contents("%s/queue/scheduler" % block.sys_path))[0]
-            result["kernel_settings_disk_read_ahead_kb"] = int(safe_file_get_contents("%s/queue/read_ahead_kb" % block.sys_path))
-            result["kernel_settings_disk_scheduler_quantum"] = safe_file_get_contents("%s/queue/iosched/quantum" % block.sys_path)
+        schedulers = safe_file_get_contents("%s/queue/scheduler" % block.sys_path)
+        if schedulers:
+            settings = re.findall(r'\[(\w+)\]', schedulers)
+            if settings:
+                kernel_settings_device_specific["kernel_settings_disk_elevator"].append({'device':block.sys_path, 'value':settings[0]})
+            kernel_settings_device_specific["kernel_settings_disk_read_ahead_kb"].append({'device':block.sys_path, 'value':safe_file_get_contents("%s/queue/read_ahead_kb" % block.sys_path)})
+            kernel_settings_device_specific["kernel_settings_disk_scheduler_quantum"].append({'device':block.sys_path, 'value':safe_file_get_contents("%s/queue/iosched/quantum" % block.sys_path)})
     
     print("get_sysfs_fields: found %d blocks associated to the template machine" % num_blocks)
-
-    result["kernel_settings_avc_cache_threshold"] = int(safe_file_get_contents("/sys/fs/selinux/avc/cache_threshold"))
-    result["kernel_settings_nf_conntrack_hashsize"] = int(safe_file_get_contents("/sys/module/nf_conntrack/parameters/hashsize"))
     
-    # will collect a list of modules associated to the template machine, but only use settings from the first one
-    num_modules = 0
-    devices = pyudev.Context().list_devices(subsystem="sound").match_sys_name("card*")
+    # will collect a list of sound cards associated to the template machine, but only use settings from the first one
+    num_sound_cards = 0
+    sound_cards = pyudev.Context().list_devices(subsystem="sound").match_sys_name("card*")
 
-    for device in devices:
-        module_name = device.parent.driver
+    kernel_settings_device_specific["kernel_settings_audio_timeout"] = []
+    kernel_settings_device_specific["kernel_settings_audio_reset_controller"] = []
+
+    for sound_card in sound_cards:
+        module_name = sound_card.parent.driver
         if module_name in ["snd_hda_intel", "snd_ac97_codec"]:
-            num_modules += 1
-            result["kernel_settings_audio_timeout"] = safe_file_get_contents("/sys/module/%s/parameters/power_save" % module_name)
-            result["kernel_settings_audio_reset_controller"] = safe_file_get_contents("/sys/module/%s/parameters/power_save_controller" % module_name)
+            num_sound_cards += 1
+            kernel_settings_device_specific["kernel_settings_audio_timeout"].append({'device':sound_card.sys_path, 'value':safe_file_get_contents("/sys/module/%s/parameters/power_save" % module_name)})
+            kernel_settings_device_specific["kernel_settings_audio_reset_controller"].append({'device':sound_card.sys_path, 'value':safe_file_get_contents("/sys/module/%s/parameters/power_save_controller" % module_name)})
 
-    print("get_sysfs_fields: found %d sound modules associated to the template machine" % num_modules)
+    print("get_sysfs_fields: found %d sound modules associated to the template machine" % num_sound_cards)
 
     # will collect a list of scsis associated to the template machine, but only use settings from the first one
     num_scsis = 0
     scsis = pyudev.Context().list_devices(subsystem="scsi")
 
+    kernel_settings_device_specific["kernel_settings_scsi_host_alpm"] = []
+
     for scsi in scsis:
         num_scsis += 1
-        if "kernel_settings_scsi_host_alpm" not in result:
-            result["kernel_settings_scsi_host_alpm"] = safe_file_get_contents("%s/link_power_management_policy" % scsi.sys_path)
+        kernel_settings_device_specific["kernel_settings_scsi_host_alpm"].append({'device':scsi.sys_path, 'value':safe_file_get_contents("%s/link_power_management_policy" % scsi.sys_path)}) 
 
     print("get_sysfs_fields: found %d scsis associated to the template machine" % num_scsis)
 
@@ -139,38 +154,44 @@ def get_sysfs_fields():
     num_gcards = 0
     gcards = pyudev.Context().list_devices(subsystem="drm").match_sys_name("card*").match_property("DEVTYPE", "drm_minor")
 
+    kernel_settings_device_specific["kernel_settings_video_radeon_powersave"] = []
+
     for gcard in gcards:
         num_gcards += 1
         method = safe_file_get_contents("%s/device/power_method" % gcard.sys_path)
-        if "kernel_settings_video_radeon_powersave" not in result:
-            if method == "profile":
-                result["kernel_settings_video_radeon_powersave"] = safe_file_get_contents("%s/device/power_profile" % gcard.sys_path)
-            elif method == "dynpm":
-                result["kernel_settings_video_radeon_powersave"] = "dynpm"
-            elif method == "dpm":
-                result["kernel_settings_video_radeon_powersave"] = "dpm-%s" % safe_file_get_contents("%s/device/power_dpm_state" % gcard.sys_path)
-    
+        if method == "profile":
+            kernel_settings_device_specific["kernel_settings_video_radeon_powersave"].append({'device':gcard.sys_path, 'value':safe_file_get_contents("%s/device/power_profile" % gcard.sys_path)}) 
+        elif method == "dynpm":
+            kernel_settings_device_specific["kernel_settings_video_radeon_powersave"].append({'device':gcard.sys_path, 'value':'dynpm'})
+        elif method == "dpm":
+            kernel_settings_device_specific["kernel_settings_video_radeon_powersave"].append({'device':gcard.sys_path, 'value':"dpm-%s" % safe_file_get_contents("%s/device/power_dpm_state" % gcard.sys_path)})
+
     print("get_sysfs_fields: found %d gcards associated to the template machine" % num_gcards)
 
     # will collect a list of usb interfaces associated to the template machine, but only use settings from the first one
     num_usbs = 0
     usbs = pyudev.Context().list_devices(subsystem="usb")
 
+    kernel_settings_device_specific["kernel_settings_usb_autosuspend"] = []
+
     for usb in usbs:
         num_usbs += 1
-        if "kernel_settings_usb_autosuspend" not in result:
-            result["kernel_settings_usb_autosuspend"] = int(safe_file_get_contents("%s/power/autosuspend" % usb.sys_path))
+        kernel_settings_device_specific["kernel_settings_usb_autosuspend"].append({'device':usb.sys_path,'value':safe_file_get_contents("%s/power/autosuspend" % usb.sys_path)})
+
+    result["kernel_settings_device_specific"] = kernel_settings_device_specific
 
     print("get_sysfs_fields: found %d usbs associated to the template machine" % num_usbs)
 
     other_sysfs = []
     ksm_dict = {"name": "/sys/kernel/mm/ksm/run"}
     ksm_dict["value"] = safe_file_get_contents("/sys/kernel/mm/ksm/run")
-    other_sysfs.append(ksm_dict)
+    if ksm_dict["value"]:
+        other_sysfs.append(ksm_dict)
 
     ktimer_dict = {"name": "/sys/kernel/ktimer_lockless_check"}
     ktimer_dict["value"] = safe_file_get_contents("/sys/kernel/ktimer_lockless_check")
-    other_sysfs.append(ktimer_dict)
+    if ktimer_dict["value"]:
+        other_sysfs.append(ktimer_dict)
 
     result["kernel_settings_sysfs"] = other_sysfs
 
