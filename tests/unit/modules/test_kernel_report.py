1f672d9152489b3bd3239b30313978872432caa7
mprovenc <mkgprov@aol.com>
2021-04-26 12:26:16 -0400

    add test for safe_file_get_contents

--- tests/unit/modules/test_kernel_report.py
+++ tests/unit/modules/test_kernel_report.py
@@ -5,12 +5,12 @@
 #
 """ Unit tests for kernel_report module """
 
-import unittest
+import unittest, sys
 
 try:
-    from unittest.mock import patch
+    from unittest.mock import patch, mock_open
 except ImportError:
-    from mock import patch
+    from mock import patch, mock_open
 
 import kernel_report
 
@@ -18,10 +18,27 @@ import kernel_report
 class KernelReportGetFields(unittest.TestCase):
     """test operations involving getting existing values from a template machine"""
 
+    if sys.version_info.major == 3:
+        builtin_module_name = 'builtins'
+    else:
+        builtin_module_name = '__builtin__'
+
+    @patch('{}.open'.format(builtin_module_name), new_callable=mock_open, read_data='1')
+    def test_safe_file_get_contents_fails_w_nonexist_file(self, mock_open):
+        mock_open.side_effect = IOError
+        rtn = kernel_report.safe_file_get_contents("junk")
+        self.assertIsNone(rtn)
+
     @patch('kernel_report.safe_file_get_contents')
     def test_get_sysctl(self, mock):
         """do various tests of get_sysctl_fields"""
-        kernel_report.get_sysctl_fields()
+        my_value = kernel_report.get_sysctl_fields()
+        print(my_value)
+
+    @patch('kernel_report.safe_file_get_contents')
+    @patch('re.findall')
+    def test_get_sysfs(self, mock_get_file, mock_re_search):
+        kernel_report.get_sysfs_fields()
 
 
 if __name__ == "__main__":
