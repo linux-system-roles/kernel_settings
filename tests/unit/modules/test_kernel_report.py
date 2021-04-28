80a9cccb1926a3d3c45129cb74d9cf3a21577fdd
mprovenc <mkgprov@aol.com>
2021-04-28 14:45:35 -0400

    add sysctl unit tests

--- tests/unit/modules/test_kernel_report.py
+++ tests/unit/modules/test_kernel_report.py
@@ -15,6 +15,14 @@ except ImportError:
 import kernel_report
 
 
+def dirty_sysctl_side_effect(*args, **kwargs):
+    if (args[0] == "/proc/sys/vm/dirty_background_bytes" or
+        args[0] == "/proc/sys/vm/dirty_background_ratio" or
+        args[0] == "/proc/sys/vm/dirty_bytes" or
+        args[0] == "/proc/sys/vm/dirty_ratio"):
+        return '0'
+    return None
+
 class KernelReportGetFields(unittest.TestCase):
     """test operations involving getting existing values from a template machine"""
 
@@ -24,16 +32,31 @@ class KernelReportGetFields(unittest.TestCase):
         builtin_module_name = '__builtin__'
 
     @patch('{}.open'.format(builtin_module_name), new_callable=mock_open, read_data='1')
-    def test_safe_file_get_contents_fails_w_nonexist_file(self, mock_open):
+    def test_safe_file_get_contents_fails_nonexist_file(self, mock_open):
         mock_open.side_effect = IOError
         rtn = kernel_report.safe_file_get_contents("junk")
         self.assertIsNone(rtn)
 
     @patch('kernel_report.safe_file_get_contents')
-    def test_get_sysctl(self, mock):
+    def test_get_sysctl_dirty_values_not_zero(self, mock_get_file):
         """do various tests of get_sysctl_fields"""
-        my_value = kernel_report.get_sysctl_fields()
-        print(my_value)
+        mock_get_file.side_effect = dirty_sysctl_side_effect
+        sysctl = kernel_report.get_sysctl_fields()
+        self.assertEqual([], sysctl["kernel_settings_sysctl"])
+
+    @patch('kernel_report.safe_file_get_contents')
+    def test_get_sysctl_no_none_values(self, mock_get_file):
+        """do various tests of get_sysctl_fields"""
+        mock_get_file.return_value = None
+        sysctl = kernel_report.get_sysctl_fields()
+        self.assertEqual([], sysctl["kernel_settings_sysctl"])
+
+    # test for kernel_settings_other to be empty when the files don't exist
+
+    # test that regex is successful for transparent/defrag fields
+
+    # test that when no cpus exist nothing is returned (tests for other modules as well)
+
 
     @patch('kernel_report.safe_file_get_contents')
     @patch('re.findall')
